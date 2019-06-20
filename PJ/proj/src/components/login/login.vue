ca4da3c5e533cf5f60aa18ccca751fed7ce6c9de
lzy565418918 <565418918@qq.com>
2019-06-20 16:47:46 +0800

    完成登录功能

---
+++ PJ/proj/src/components/login/login.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="login">
+    <el-form
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+      label-position="top"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名" prop="pass">
+        <el-input type="text" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      }
+      callback();
+    };
+    return {
+      ruleForm: {
+        pass: "",
+        checkPass: ""
+      },
+      rules: {
+        pass: [{ required: true, validator: validatePass, trigger: "blur" }],
+        checkPass: [
+          { required: true, validator: validatePass2, trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        console.log(this.ruleForm.pass);
+        console.log(this.ruleForm.checkPass);
+
+        if (valid) {
+          this.$http({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: {
+              username: this.ruleForm.pass,
+              password: this.ruleForm.checkPass
+            }
+          }).then(res => {
+            console.log(res.data.meta);
+
+            let { msg, status } = res.data.meta;
+            if (status === 200) {
+              this.$message({
+                showClose: true,
+                message: msg,
+                type: "success"
+              });
+              setTimeout(() => {
+                this.$router.push("/home");
+              }, 3000);
+            } else {
+              this.$message({
+                showClose: true,
+                message: "用户名或密码错误",
+                type: "error"
+              });
+            }
+          });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style>
+.login {
+  width: 100%;
+  height: 100%;
+  background-color: #324152;
+  position: relative;
+}
+.el-form {
+  width: 580px;
+  height: 440px;
+  box-sizing: border-box;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 40px;
+  border-radius: 5px;
+}
+.el-button {
+  width: 100%;
+}
+</style>
--- PJ/proj/src/main.js
+++ PJ/proj/src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
