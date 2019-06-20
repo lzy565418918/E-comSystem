f2e61aacf0313f57f4455a1d5cd1c81f5d8b6b8b
lzy565418918 <565418918@qq.com>
2019-06-20 17:10:45 +0800

    主页开始

--- PJ/proj/src/router/index.js
+++ PJ/proj/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/components/login/login'
+import home from '@/components/home/home'
 import '@/assets/css/index.css'
 
 Vue.use(Router)
@@ -11,6 +12,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: login
+    },
+    {
+      path: '/home',
+      name: '/home',
+      component: home
     }
   ]
 })
